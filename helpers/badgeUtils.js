da473edbaf9330f967d8ad7d7e94b63ae42d3b5d
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2025-09-16 22:48:32 -0500

    fix badge getting (and also cache them)

--- helpers/badgeUtils.js
+++ helpers/badgeUtils.js
@@ -4,19 +4,25 @@ const BADGE_TIERS = {
     PURPLE: 3,
 };
 
+let allBadgesCache = null;
+
 function getAllBadges() {
+    if (allBadgesCache) return allBadgesCache;
+
     const badges = [];
     const badgeFiles = require('fs').readdirSync('./badges').filter(f => f.endsWith('.js'));
     for (const file of badgeFiles) {
         const badgeObj = require(`../badges/${file}`)();
         badges.push(badgeObj);
     }
+    allBadgesCache = badges;
 
     return badges;
 }
 
 function getBadgeByName(name) {
-    const badgeObj = require(`../badges/${name}.js`)();
+    const allBadges = getAllBadges();
+    const badgeObj = allBadges.find(b => b.name.toLowerCase() === name.toLowerCase() || (b.aliases && b.aliases.map(a => a.toLowerCase()).includes(name.toLowerCase())));
     if (!badgeObj) {
         console.warn(`[WARN] tried to get badge ${name} but it doesn't exist`);
         return null;

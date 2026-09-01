68592cdb7254054048b243a9bb2bd044a16520c0
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2026-09-01 14:37:37 +0000

    improve error logs again

--- helpers/log.js
+++ helpers/log.js
@@ -6,13 +6,29 @@ module.exports = async (error, client, rawError) => {
     console.log(`[ERROR] ${error}`, rawError)
 
     const user = await client.users.fetch(ownerId);
+    const attachments = [];
+    const attachmentsRef = [];
+    const errorTooLong = `${error}`.length > 1000;
 
+    if (errorTooLong) {
+        fs.appendFileSync('error.txt', `${error}\n\n${rawError ? rawError.stack : ""}`);
+        const attachment = new AttachmentBuilder('error.txt');
+        attachments.push(attachment);
+        attachmentsRef.push('error.txt');
+    }
     if (rawError && rawError.stack) {
         fs.appendFileSync('errorLog.txt', `${rawError ? rawError.stack : ""}`);
         const attachment = new AttachmentBuilder('errorLog.txt');
-        await user.send({ content: `[ERROR] ${error}`, files: [attachment] })
-        fs.rmSync('errorLog.txt');
-    } else {
-        await user.send(`[ERROR] ${error}`);
+        attachments.push(attachment);
+        attachmentsRef.push('errorLog.txt');
+    } 
+    
+    await user.send({ 
+        content: `[ERROR] ${errorTooLong ? "(See file)" : error}`,
+        files: attachments
+    })
+
+    for (const file of attachmentsRef) {
+        fs.rmSync(file);
     }
 }
